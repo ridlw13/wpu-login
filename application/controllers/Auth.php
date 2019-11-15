6420ab06a29b2c97fbad0ab55c4b62723e919d64
ridlw13 <ridlwanbaks@mail.unpas.ac.id>
2019-11-15 08:24:38 +0700

    First Commit

---
+++ application/controllers/Auth.php
@@ -0,0 +1,14 @@
+<?php 
+
+      defined('BASEPATH') or exit('No direct script access allowed');
+
+      class Auth extends CI_Controller 
+      {
+            public function index()
+            {
+                  $this->load->view('auth/login');
+            }
+      }
+      
+
+?>
