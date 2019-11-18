279715557002ceae80b9faeac6077cd1ad101da8
ridlw13 <ridlwanbaks@mail.unpas.ac.id>
2019-11-18 10:09:04 +0700

    Menambahkan fitur login dan cek apakah email dan password sesuai atau tidak

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -11,16 +11,24 @@
             }
             public function index()
             {
-                  $data['tittle'] = 'Login Page';
-                  $this->load->view('templates/auth_header', $data);
-                  $this->load->view('auth/login');
-                  $this->load->view('templates/auth_footer');
+                  $this->form_validation->set_rules('email','Email', 'trim|required|valid_email');
+                  $this->form_validation->set_rules('password', 'Password', 'trim|required');
+
+                  if ($this->form_validation->run() == false) {
+                        $data['tittle'] = 'Login Page';
+                        $this->load->view('templates/auth_header', $data);
+                        $this->load->view('auth/login');
+                        $this->load->view('templates/auth_footer');
+                   }else{
+                         $this->_login();
+                   }
+                  
             }
 
             public function registration(){
 
                   $this->form_validation->set_rules('name', 'Name', 'required|trim');
-                  $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email', [
+                  $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
                         'is_unique' => 'This email has already registered!'
                   ]);
                   $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]', [
@@ -52,7 +60,43 @@
                   }
                   
             }
-      }
-      
 
-?>
+            private function _login()
+            {
+                  $email = $this->input->post('email');
+                  $password = $this->input->post('password');
+
+                  $user = $this->db->get_where('user', ['email' => $email])->row_array();
+
+                  if ($user) {
+                        if ($user['is_active'] == 1) {
+                              if (password_verify($password, $user['password'])) {
+                                    $data = [
+                                          'email' => $user['email'],
+                                          'role_id' => $user['role_id']
+                                    ];
+                                    $this->session->set_userdata($data);
+                                    redirect('user');
+                              }else {
+                                    $this->session->set_flashdata('message', '<div class = "alert alert-danger" role ="alert"> Wrong password!</div>');
+                                    redirect('auth');
+                              }
+                        }else{
+                              $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">This email has not been activated!</div>');
+                              redirect('auth');
+                  } 
+            }else{
+                  $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Email is not registered!</div>');
+                  redirect('auth');
+            }
+      }
+
+      public function logout()
+      {
+            $this->session->unset_userdata('email');
+            $this->session->unset_userdata('role_id');
+
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role ="alert">You have been logged out!</div>');
+            redirect('auth');
+      }
+}
--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -18,12 +18,14 @@
                     <h1 class="h4 text-gray-900 mb-4">Login Page</h1>
                   </div>
                   <?= $this->session->flashdata('message');?>
-                  <form class="user">
+                  <form class="user" method="post" action="<?= base_url('auth'); ?>">
                     <div class="form-group">
-                      <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
+                      <input type="email" class="form-control form-control-user" id="email" name="email"  placeholder="Enter Email Address..." value="<?= set_value('email');?>">
+                      <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
                     </div>
                     <div class="form-group">
-                      <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
+                      <input type="password" class="form-control form-control-user" id="password" name="password" placeholder="Password">
+                      <?= form_error('password', '<small class="text-danger pl-3">', '</small>'); ?>
                     </div>
                     <div class="form-group">
                       <div class="custom-control custom-checkbox small">
@@ -31,9 +33,9 @@
                         <label class="custom-control-label" for="customCheck">Remember Me</label>
                       </div>
                     </div>
-                    <a href="index.html" class="btn btn-primary btn-user btn-block">
-                      Login
-                    </a>
+                    <button type="submit" class="btn btn-primary btn-user btn-block">
+                  Login
+                    </button>
                     <hr>
                     <a href="index.html" class="btn btn-google btn-user btn-block">
                       <i class="fab fa-google fa-fw"></i> Login with Google
