a4d84d63228ee7aaed078642dcfcfd9f0d7c928a
ridlw13 <ridlwanbaks@mail.unpas.ac.id>
2019-11-18 11:12:40 +0700

    Menambahkan halaman user dan menambahkan fitur logout

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -6,8 +6,11 @@ class User extends CI_Controller
 {
       public function index()
       {
+            $data['title'] = 'My Profile';
             $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
-            echo 'Selamat datang ' . $data['user']['name'];
+            
+
+            $this->load->view('user/index', $data);
       }
 }
 
