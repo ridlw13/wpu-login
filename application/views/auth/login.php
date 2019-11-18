1bdb791eebaf1d8ca4c2e15e2ac2dadc03b4d85b
ridlw13 <ridlwanbaks@mail.unpas.ac.id>
2019-11-18 09:22:02 +0700

    Membuat fitur registrasi akun baru untuk user

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -17,6 +17,7 @@
                   <div class="text-center">
                     <h1 class="h4 text-gray-900 mb-4">Login Page</h1>
                   </div>
+                  <?= $this->session->flashdata('message');?>
                   <form class="user">
                     <div class="form-group">
                       <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
@@ -46,7 +47,7 @@
                     <a class="small" href="forgot-password.html">Forgot Password?</a>
                   </div>
                   <div class="text-center">
-                    <a class="small" href="register.html">Create an Account!</a>
+                    <a class="small" href="<?= base_url();?>/auth/registration">Create an Account!</a>
                   </div>
                 </div>
               </div>
